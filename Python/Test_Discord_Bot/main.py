5587e6570ad6ac82ac56f4dfd2704fb57fff8326
m-smith15 <mpsmith1215@gmail.com>
2022-07-27 16:16:38 -0400

    wording update commit

--- Python/Test_Discord_Bot/main.py
+++ Python/Test_Discord_Bot/main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+import datetime
 load_dotenv('.env')
 
 client = discord.Client()
@@ -23,7 +24,18 @@ async def on_message(message):
     if message.content.startswith('hey guys its me gupi'):
         await message.channel.send('let me guess: PINCHY ALERT OMG!!!!!!!!')
 
+    if message.content.startswith('hey guys its me jrollman123'):
+        await message.channel.send('ijr')
+
     if message.content.endswith('price'):
         await message.channel.send('the price is right')
+    
+    if message.content.endswith('divinity') or message.content.startswith('divinity'):
+        dCheck = message.created_at
+        if dCheck < datetime.datetime(2022, 7, 27, 23, 0, 00):
+            await message.channel.send('Not time yet')
+        elif dCheck > datetime.datetime(2022,7,27, 23, 0, 00):
+            await message.channel.send('TRYNA?!')
+
 
 client.run(os.getenv('TOKEN'))
