c882e0293e33e2fd276ca4ea2661848e7a978446
m-smith15 <mpsmith1215@gmail.com>
2022-08-08 10:49:05 -0400

    added server api, front-end updates

--- Python/wow_ah_project/flask_app/controllers/search.py
+++ Python/wow_ah_project/flask_app/controllers/search.py
@@ -30,38 +30,60 @@ def search_page():
 
     print("Bearer is " + os.environ['Bearer'])
 
-    return render_template("index.html")
+    #adding in an api call for realm list to build options
+    realmListAPI = requests.get(f"https://us.api.blizzard.com/data/wow/realm/index?namespace=dynamic-classic-us&locale=en_US&access_token={os.environ.get('Bearer')}")
+    print("API realm call complete")
+
+    realmListJSON = realmListAPI.json()
+
+    #print(realmListJSON)
+
+    realmList = []
+
+    for realm in realmListJSON['realms']:
+        if realm['name'].startswith('US1') or realm['name'].startswith('US2'): {
+        }
+        else: {
+        realmList.append([realm['name'], realm['id']])
+        }
+
+    sortedRealmList = sorted(realmList, key=lambda d: d[0])
+    print(sortedRealmList)
+    #print(realmList[1][1])
+
+    return render_template("index.html", realmList = sortedRealmList)
 
 @app.route('/results', methods=['POST'])
 def searching():
     # print(request.form['query']) #returns as str by default
     itemSearched = request.form['query']
+    serverChosen = request.form['selectRealm']
 
     #Leaving this in for now. Will be tricky based on how the search API works for WoW. Seems like you get *Like results of what you search from blizz api
     searchItem = requests.get(f"https://us.api.blizzard.com/data/wow/search/item?namespace=static-us&name.en_US={itemSearched}&orderby=id&_page=1&access_token={os.environ.get('Bearer')}")
     print("Search API complete")
 
-    dict = requests.get(f"https://us.api.blizzard.com/data/wow/connected-realm/4728/auctions/2?namespace=dynamic-classic-us&locale=en_US&access_token={os.environ.get('Bearer')}").json()
+    dict = requests.get(f"https://us.api.blizzard.com/data/wow/connected-realm/{serverChosen}/auctions/2?namespace=dynamic-classic-us&locale=en_US&access_token={os.environ.get('Bearer')}").json()
     print("Auction API call complete")
     results = []
     resultsQuantity = 0
-    resultsAverage = 0
+    resultsTotal = 0
     #looping through all auctions in API for ID of item searched
     for auction in dict['auctions']:
         if auction['item']['id'] == int(itemSearched):
             resultsQuantity += 1
             auction['buyout'] = round(auction['buyout']/10000,2)
-            resultsAverage += auction['buyout']
+            resultsTotal += auction['buyout']
             results.append(auction)
 
     #rounding total results
-    resultsAverage = round(resultsAverage, 2)
+    resultsAverage = round(resultsTotal / resultsQuantity, 2)
     print(resultsAverage)
 
 
     sortedResults = sorted(results, key=lambda d: d['buyout'])
-    resultsJson = json.dumps(results)
-    print(sortedResults)
+    #resultsJson = json.dumps(results)
+    #print(sortedResults)
     # we must keep in line with JSON format.
     # requests has a method to convert the data coming back into JSON.
     return render_template("results.html", results = sortedResults, quantity = resultsQuantity, itemSearched = itemSearched, average = resultsAverage)
