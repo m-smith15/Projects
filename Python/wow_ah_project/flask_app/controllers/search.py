e35d0bea58d9899893d82aafb5b99cdfe52cd3ed
m-smith15 <mpsmith1215@gmail.com>
2022-08-20 02:40:32 -0400

    playing with pagination, updated tooltip display

--- Python/wow_ah_project/flask_app/controllers/search.py
+++ Python/wow_ah_project/flask_app/controllers/search.py
@@ -3,6 +3,7 @@ import requests
 import os
 from flask import render_template, redirect, session, jsonify, request, json
 from flask_app import app
+from flask_paginate import Pagination, get_page_parameter
 
 @app.route('/')
 def search_page():
@@ -48,6 +49,7 @@ def search_page():
         }
 
     sortedRealmList = sorted(realmList, key=lambda d: d[0])
+    
     print(sortedRealmList)
     #print(realmList[1][1])
 
@@ -58,12 +60,14 @@ def searching():
     # print(request.form['query']) #returns as str by default
     itemSearched = request.form['query']
     serverChosen = request.form['selectRealm']
+    # 2 = alliance 6 = horde 7 = bbah
+    factionChosen = request.form['selectFaction']
 
     #Leaving this in for now. Will be tricky based on how the search API works for WoW. Seems like you get *Like results of what you search from blizz api
     searchItem = requests.get(f"https://us.api.blizzard.com/data/wow/search/item?namespace=static-us&name.en_US={itemSearched}&orderby=id&_page=1&access_token={os.environ.get('Bearer')}")
     print("Search API complete")
 
-    dict = requests.get(f"https://us.api.blizzard.com/data/wow/connected-realm/{serverChosen}/auctions/2?namespace=dynamic-classic-us&locale=en_US&access_token={os.environ.get('Bearer')}").json()
+    dict = requests.get(f"https://us.api.blizzard.com/data/wow/connected-realm/{serverChosen}/auctions/{factionChosen}?namespace=dynamic-classic-us&locale=en_US&access_token={os.environ.get('Bearer')}").json()
     print("Auction API call complete")
     results = []
     resultsQuantity = 0
@@ -79,9 +83,25 @@ def searching():
     #rounding total results
     resultsAverage = round(resultsTotal / resultsQuantity, 2)
     print(resultsAverage)
+    print(int(resultsTotal))
 
 
     sortedResults = sorted(results, key=lambda d: d['buyout'])
+    page = request.args.get('page')
+    print(sortedResults)
+
+# ----------- pagination building 
+
+    # if page and page.isdigit():
+    #     page = int(page)
+    # else:
+    #     page = 1
+    # sortedResults = sortedResults.paginate(page=page, per_page=10)
+    # pagination = Pagination(page=page, total=int(resultsTotal), search= True, record_name='query results')
+    # print(sortedResults)
+    # print(pagination.items)
+
+
     #resultsJson = json.dumps(results)
     #print(sortedResults)
     # we must keep in line with JSON format.
