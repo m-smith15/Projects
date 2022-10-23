3dbc328acf40357f7f477f7e8f0d8c903609a831
m-smith15 <mpsmith1215@gmail.com>
2022-10-23 03:35:19 -0400

    updating of backend for login page

--- Python/wow_ah_project/flask_app/models/users.py
+++ Python/wow_ah_project/flask_app/models/users.py
@@ -1,44 +1,70 @@
 from flask_app.config.mysqlconnection import connectToMySQL 
+from flask import flash, request
+import re
 #since this is MODEL need to reach out to DB
 
-# import tables from users schema
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+# import tables from mydb (schema)
 class User:
-    def __init__(self,data):
-        self.id = data['id']
-        self.first_name = data['first_name']
-        self.last_name = data['last_name']
-        self.email = data['email']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
+    def __init__(self, data):
+        self.id = data["id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.email = data["email"]
+        self.password = data["password"]
 
     @classmethod
-    def get_all(cls):
-        query = "SELECT * FROM users;"
-        results = connectToMySQL('users_schema').query_db(query)
-        users = []
-        # Iterate over the db results and create instances of users with cls.
-        for user in results:
-            users.append( cls(user) )
-        return users
+    def create_user(cls, user_data):
 
-    @classmethod
-    def save(cls, data ):
-        query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(first_name)s , %(last_name)s , %(email)s , NOW() , NOW() );"
-        # data was created in users controller > we're using the values gathered there to insert into database
-        return connectToMySQL('users_schema').query_db( query, data )
+        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
 
+        return connectToMySQL('mydb').query_db(query, user_data)
+    
     @classmethod
-    def delete(cls, data):
-        query = "DELETE FROM users WHERE (id = %(id)s);"
-        return connectToMySQL('users_schema').query_db( query, data )
+    def validate_login(cls, user_data):
 
-    @classmethod
-    def show_one(csl, data):
-        query = "Select * FROM users where (id=%(id)s);"
-        return connectToMySQL('users_schema').query_db( query, data )
+        query = "SELECT * FROM users WHERE email = %(email)s"
+        results = connectToMySQL('mydb').query_db(query, user_data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
 
-    @classmethod
-    def edit(csl, data):
-        query = "UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s WHERE id=%(id)s;"
-        return connectToMySQL('users_schema').query_db( query, data )
+    @staticmethod
+    def validate_registration():
+        is_valid = True
+        if len(request.form["first_name"]) < 3:
+            flash("Name must be at least 2 characters long!")
+            is_valid = False
+        if len(request.form["last_name"]) < 3:
+            flash("Last Name must be at least 2 characters long!")
+            is_valid = False
+        if not EMAIL_REGEX.match(request.form["email"]):
+            flash("Must use a valid email!")
+            is_valid = False
+        if len(request.form["password"]) < 3:
+            flash("pasword less than 3 - replace with bcrypt")
+            is_valid = False
+        return is_valid
+
+    @staticmethod
+    def existing_user_check(email_check):
+        is_valid = True
+
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+
+        results = connectToMySQL('mydb').query_db(query, email_check)
+
+        print(len(results))
+
+        if len(results) != 0:
+            flash("That email is already in use")
+            is_valid = False
+
+        if request.form["password"] != request.form["password_confirm"]:
+            flash("Passwords must match!")
+            is_valid = False
+
+        return is_valid
+
 
