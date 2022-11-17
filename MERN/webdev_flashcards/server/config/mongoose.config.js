b70dd1c95a8d8ea15105954f41e522b5b9bc5a0b
m-smith15 <mpsmith1215@gmail.com>
2022-11-16 22:25:57 -0500

    restructuring - moved to separate repo

--- MERN/webdev_flashcards/server/config/mongoose.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb://localhost/webdev_flashcardsdb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log("Connected with the DB!"))
-    .catch(err => console.log("Got an error connecting to DB", err));
-//creating if not present, and logging connection to mongodb 
--- MERN/webdev_flashcards/server/models/card.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const CardSchema = new mongoose.Schema({
-    cardTitle: {type: String},
-    cardDescription: {type: String}
-}, { timestamps: true } );
-//setup the schema w/ mongoose, also set some basic validations
-module.exports.Card = mongoose.model('Card', CardSchema);
-//dont forget to export
