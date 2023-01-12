eb48cc7b30854f597696fa1a65279091e013d570
m-smith15 <mpsmith1215@gmail.com>
2023-01-12 18:50:14 -0500

    updating user model following planetscale move

--- Python/wow_ah_project/flask_app/models/users.py
+++ Python/wow_ah_project/flask_app/models/users.py
@@ -1,11 +1,11 @@
 from flask_app.config.mysqlconnection import connectToMySQL 
-from flask import flash, request
+from flask import flash, request, json
 import re
 #since this is MODEL need to reach out to DB
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
-# import tables from mydb (schema)
+# import tables from oah_db (schema)
 class User:
     def __init__(self, data):
         self.id = data["id"]
@@ -19,16 +19,23 @@ class User:
 
         query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
 
-        return connectToMySQL('mydb').query_db(query, user_data)
+        return connectToMySQL('oah_db').query_db(query, user_data)
     
     @classmethod
     def validate_login(cls, user_data):
-
         query = "SELECT * FROM users WHERE email = %(email)s"
-        results = connectToMySQL('mydb').query_db(query, user_data)
+        results = connectToMySQL('oah_db').query_db(query, user_data)
         if len(results) < 1:
             return False
-        return cls(results[0])
+        #need to move results list to a dict so a user class can be instantiated
+        results_dict = {
+            'id': results[0][0],
+            'first_name': results[0][1],
+            'last_name': results[0][2],
+            'email':results[0][3],
+            'password': results[0][4]
+        }
+        return cls(results_dict)
 
     @staticmethod
     def validate_registration():
@@ -53,7 +60,7 @@ class User:
 
         query = "SELECT * FROM users WHERE email = %(email)s;"
 
-        results = connectToMySQL('mydb').query_db(query, email_check)
+        results = connectToMySQL('oah_db').query_db(query, email_check)
 
         print(len(results))
 
